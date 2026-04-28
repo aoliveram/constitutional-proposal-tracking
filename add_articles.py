683f391e2346f6e30c1b1379797b87c1827dc138
Vicente <vcl.lopeztorres@gmail.com>
2026-04-28 14:54:09 -0400

    Se creó el primer génesis y se integró el primer informe de indicaciones. Hubo un par de indicaciones aprobadas que no se encontraron en el JSON de 03-02, pero se ingresaron manualmente.

--- add_articles.py
+++ add_articles.py
@@ -1,6 +1,6 @@
 import json
 
-file_path = r'c:\Users\vicel\Proyectos\constitutional-proposal-tracking\comision-5\draft-after-indications-manual\C5_historial_manual.json'
+file_path = r'C:\Users\vicel\Proyectos\constitutional-proposal-tracking\comision-2\draft-after-indications-manual\C2_historial_manual.json'
 
 try:
     with open(file_path, 'r', encoding='utf-8') as f:
@@ -8,8 +8,8 @@ try:
     
     for i in range(53, 359):
         new_article = {
-            "article_uid": f"C5-GEN2-CH1-ART{i}",
-            "timestamp": "03-26",
+            "article_uid": f"C2_GEN_ART{i}",
+            "timestamp": "02-16",
             "article": f"Artículo {i}",
             "text": " ",
             "sources": [
@@ -21,6 +21,6 @@ try:
     with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
         
-    print(f"Successfully appended articles 53 to 358 to the JSON file.")
+    print(f"Successfully appended to the JSON file.")
 except Exception as e:
     print(f"Error: {e}")
